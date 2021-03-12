dee390019d1b6f37b9f91c99a40e94c7027cb721
LFSCamargo <luizepauloxd@gmail.com>
2021-03-12 14:26:59 -0300

    feat: adds useLoading hook

---
+++ src/hooks/useToken.ts
@@ -0,0 +1,23 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useCallback, useEffect, useState } from "react";
+
+export function useToken() {
+  const [loading, setLoading] = useState(false);
+  const [token, setToken] = useState("");
+
+  const getData = useCallback(async () => {
+    setLoading(true);
+    const token = (await AsyncStorage.getItem("token")) || "";
+    setToken(token);
+    setLoading(false);
+  }, []);
+
+  useEffect(() => {
+    getData;
+  }, []);
+
+  return {
+    token,
+    loading,
+  };
+}
