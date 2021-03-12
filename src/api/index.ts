3cda54e2f5a8a6e81c298a696a677827dd89cd52
LFSCamargo <luizepauloxd@gmail.com>
2021-03-12 13:56:01 -0300

    feat: adds api requests

---
+++ src/api/index.ts
@@ -0,0 +1,51 @@
+import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const api = axios.create({
+  baseURL: "http://localhost:3000",
+});
+
+api.interceptors.request.use(async (config) => {
+  const token: string | null = await AsyncStorage.getItem("token");
+
+  return {
+    ...config,
+    headers: {
+      ...config.headers,
+      authorization: token,
+    },
+  };
+});
+
+export type User = {
+  name: string;
+  email: string;
+  _id: string;
+  birthDate: string;
+};
+
+export async function getLoggedUser() {
+  const { data } = await api.get("/me");
+  return data;
+}
+
+type AuthOutput = {
+  token: string;
+};
+
+export async function loginUser(params: { email: string; password: string }) {
+  const { data } = await api.post<AuthOutput>("/login", {
+    ...params,
+  });
+  return data;
+}
+
+export async function signUpUser(params: {
+  email: string;
+  name: string;
+  birthDate: string;
+  password: string;
+}) {
+  const { data } = await api.post<AuthOutput>("/register");
+  return data;
+}
